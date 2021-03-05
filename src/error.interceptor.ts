6e3532a4b2eed1ad428dad967fe5e07094f224f0
Liam <liam@Liamui-MacBookPro.local>
2021-03-05 11:48:36 +0900

    v0.0.3

--- src/error.interceptor.ts
+++ src/error.interceptor.ts
@@ -1,19 +1,18 @@
 import {
-    Injectable,
-    NestInterceptor,
-    ExecutionContext,
-    BadGatewayException,
-    CallHandler,
-  } from '@nestjs/common';
-  import { Observable, throwError } from 'rxjs';
-  import { catchError } from 'rxjs/operators';
-  
-  @Injectable()
-  export class ErrorsInterceptor implements NestInterceptor {
-    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-      return next
-        .handle()
-        .pipe(catchError((err) => throwError(new BadGatewayException(err))));
-    }
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  BadGatewayException,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class ErrorsInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next
+      .handle()
+      .pipe(catchError((err) => throwError(new BadGatewayException(err))));
   }
-  
+}
